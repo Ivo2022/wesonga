f422d3717bd15b4cd11f564ba6896d2382f37226
Ivo2022 <ivan.wambette@gmail.com>
2022-06-07 14:00:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// import Header from './Header'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import { Router } from 'react-router';
 import './App.css';
+import Header from './Header'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+// import Contact from './Contact';
+// import Checkout from './Checkout';
+// import Navlinks from './Navlinks';
+
+
+// import Login from './Login'
+import Home from './Home'
+// import Footer from './Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" caseSensitive={false} element={<><Header/></>} /> */}
+        {/* <Route path="/checkout" caseSensitive={false} element={<Checkout />} />
+        <Route path="/contact" caseSensitive={false} element={<Contact />} /> */}
+         <Route path="/" caseSensitive={false} element={<><Header/><Home/></>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css"
+
+    function Header(){
+    return(
+        <nav className="header">
+            <h1>Ronald Wesonga</h1>
+        </nav>
+    )
+}
+
+export default Header;
